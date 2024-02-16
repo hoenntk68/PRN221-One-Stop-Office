78d1023b012f180e62cbe3cae9084cf556b9a3b5
AnnaTrieu <anlalahello@gmail.com>
2024-02-16 20:00:28 +0700

    submit request be

--- OneStopOfficeBE/OneStopOfficeBE/Services/Impl/RequestServiceImpl.cs
+++ OneStopOfficeBE/OneStopOfficeBE/Services/Impl/RequestServiceImpl.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Options;
 using OneStopOfficeBE.DTOs.Request;
 using OneStopOfficeBE.DTOs.Response;
 using OneStopOfficeBE.Models;
+using OneStopOfficeBE.Constants;
 using System.Security.Cryptography.X509Certificates;
 
 namespace OneStopOfficeBE.Services.Impl
@@ -27,12 +28,58 @@ namespace OneStopOfficeBE.Services.Impl
         public BaseResponse getRequest(string id)
         {
             return BaseResponse.ofSucceeded(_context.Requests.ToList());
-            //return BaseResponse.ofSucceeded(new { Name = "test", file = "test file", reason = "reson" });
         }
 
         public BaseResponse submitRequest(SubmitRequestDto submitRequest)
         {
-            return BaseResponse.ofSucceeded(submitRequest);
+            try
+            {
+                if (submitRequest == null)
+                {
+                    return BaseResponse.ofFailed("Invalid request data.");
+                }
+
+                int category = submitRequest.category;
+                string reason = submitRequest.reason;
+                IFormFile attachment = submitRequest.attachment;
+                string attachmentFilePath = SaveAttachment(attachment);
+
+                var request = new Request
+                {
+                    CategoryId = category,
+                    Reason = reason,
+                    Attachment = attachmentFilePath
+                };
+
+                _context.Requests.Add(request);
+                _context.SaveChanges();
+
+                return BaseResponse.ofSucceeded(request);
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = $"An error occurred while processing the request: {ex.GetType()}: {ex.Message}\n{ex.StackTrace}";
+
+                return BaseResponse.ofFailed(errorMessage);
+            }
+
+
+        }
+
+        private string SaveAttachment(IFormFile attachment)
+        {
+            string attachmentFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, RequestConstant.REQUEST_ATTACHMENT_PATH);
+            if (!Directory.Exists(attachmentFolderPath))
+            {
+                Directory.CreateDirectory(attachmentFolderPath);
+            }
+            string attachmentFileName = Guid.NewGuid().ToString() + "_" + attachment.FileName;
+            string attachmentFilePath = Path.Combine(attachmentFolderPath, attachmentFileName);
+            using (var stream = new FileStream(attachmentFilePath, FileMode.Create))
+            {
+                attachment.CopyTo(stream);
+            }
+            return attachmentFilePath;
         }
     }
 }

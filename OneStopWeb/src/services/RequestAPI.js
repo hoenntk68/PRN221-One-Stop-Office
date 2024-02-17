524e24b15ea66953bf2e77a31701c81fcd307935
Khanh Huyen Nguyen <hn8319542@gmail.com>
2024-02-17 17:57:30 +0700

    feat(request-list): getting request list in progress...

--- OneStopWeb/src/services/RequestAPI.js
+++ OneStopWeb/src/services/RequestAPI.js
@@ -2,9 +2,9 @@ import BaseService from "./BaseService";
 import API_CODE from "../utils/api_code.js";
 
 export default class RequestAPI extends BaseService {
-    async getRequest(params = {}, success, error) {
-        await this.post(API_CODE.API_RQ_001, params, success, error);
-    }
 
+    getRequestList(params = {}, success, error) {
+        this.get(API_CODE.API_RQ_001, success, error, params);
+    }
 
 }
---
+++ OneStopWeb/src/views/RequestListView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <RequestTable :requestList="anything"></RequestTable>
+    <button @click="getRequestList">Get Request List</button>
+  </div>
+</template>
+<script>
+import RequestTable from '@/components/RequestTable.vue'
+import { useRequestStore } from '@/stores/request.js'
+import { computed, onMounted } from 'vue'
+export default {
+  setup() {
+    // Call API get request list
+    const requestStore = useRequestStore()
+
+    const { getRequestList } = requestStore
+
+    const anything = computed(() => requestStore.requestList.data)
+
+    onMounted(async () => {
+      await getRequestList()
+    })
+
+    return {
+      getRequestList,
+      anything
+    }
+  },
+  components: { RequestTable }
+}
+</script>
+<style></style>
